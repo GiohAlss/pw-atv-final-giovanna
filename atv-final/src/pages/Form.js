893da62c30ac08826ce50a80045abb6c08a91c9b
GiohAlss <giovanna.almeida074@gmail.com>
2024-04-15 22:03:39 -0300

    Atividade 5: Implementação do formulário

--- atv-final/src/pages/Form.js
+++ atv-final/src/pages/Form.js
@@ -8,10 +8,11 @@ import Select from '../components/form/Select';
 function Form() {
     
     const [categories, setCategories] = useState([]);
+    const [classes, setClasses] = useState({});
 
     useEffect(() =>{
         fetch(
-            'http://localhost:3000/class_categories',
+            'http://localhost:5000/class_categories',
             {
                 method:'GET',
                 headers:{
@@ -32,13 +33,55 @@ function Form() {
             )
         }, [])
 
+        function handlerChangeClasses(event) {
+            setClasses({...classes, [event.target.name] : event.target.value});
+            console.log(classes)
+        }
+
+        function handlerChangeCategory(event) {
+            setClasses({...classes, category:{
+                id: event.target.value,
+                category: event.target.options[event.target.selectedIndex].text
+            }});
+        }
+
+        console.log(classes)
+
+        function createStudent(classes) {
+            fetch('http://localhost:5000/class', {
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(classes)
+            })
+            .then(
+                (resp)=>resp.json()
+            )
+            .then(
+                (data)=>{
+                    console.log(data)
+                }
+            )
+            .catch(
+                (err)=>{
+                    console.log(err)
+                }
+            )
+        } 
+
+        function submit(event){
+            event.preventDefault();
+            createStudent(classes)
+        }
+
     return(
        <section className={styles.form_container}>
             <h1>Formulário de Turmas</h1>
-            <form>
+            <form onSubmit={submit}>
 
-                <Input type='text' name='name_student' id='name_student' text='Nome do Aluno(a)' placeholder='Digite o nome do aluno(a)' />
-                <Select name='class_id' text='Selecione a turma do aluno(a)' options={categories} />
+                <Input type='text' name='name_student' id='name_student' text='Nome do Aluno(a)' placeholder='Digite o nome do aluno(a)' handlerOnchange={handlerChangeClasses} />
+                <Select name='class_id' text='Selecione a turma do aluno(a)' options={categories} handlerOnchange={handlerChangeCategory} />
 
                 <p>
                     <input type='submit' value='Cadastrar Aluno(a)' />
